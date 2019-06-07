51b69161c18143385fe33833a3f5e4531d235c68
Kostia Drobotovych <k.drobotovych@gmail.com>
2019-06-07 03:45:25 +0300

    Updates

---
+++ precache-manifest.c2ea17566e462d1f65bb7a1f8fdcf4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cb02282695942d8f67a",
+    "url": "/weaher/static/js/runtime~main.1758487e.js"
+  },
+  {
+    "revision": "3bcbe68e0156f93f9a45",
+    "url": "/weaher/static/js/main.d8750ce9.chunk.js"
+  },
+  {
+    "revision": "5a64cab7a91eec491627",
+    "url": "/weaher/static/js/2.f478b519.chunk.js"
+  },
+  {
+    "revision": "3bcbe68e0156f93f9a45",
+    "url": "/weaher/static/css/main.b82a772a.chunk.css"
+  },
+  {
+    "revision": "edbe7ca9db5c8297c160f973193f759b",
+    "url": "/weaher/index.html"
+  }
+];
